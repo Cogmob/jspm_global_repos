73fbf2f2235621f413b56a4220b6a57c6b12c2d4
Lambda Games <lambda.games.mail@gmail.com>
2017-03-01 18:16:54 +0000

    added a couple of imports

--- install.sh
+++ install.sh
@@ -47,3 +47,5 @@ node node_modules/jspm/cli.js install npm:yamljs
 node node_modules/jspm/cli.js install npm:json2yaml
 node node_modules/jspm/cli.js install npm:wordwrap
 node node_modules/jspm/cli.js install npm:glob
+jspm install npm:import-glob
+jspm install npm:babel-loader
